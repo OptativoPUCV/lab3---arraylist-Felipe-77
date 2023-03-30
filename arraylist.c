11fcf554aad456c4f65e29acc061c41fb32629b3
Your Name <you@example.com>
2023-03-30 16:16:48 -0300

    new try

--- arraylist.c
+++ arraylist.c
@@ -12,43 +12,45 @@ typedef struct ArrayList {
 
 ArrayList *createList(void) {
 
-    ArrayList * nuevaLista = (ArrayList *) malloc(sizeof(ArrayList));
-    if (!nuevaLista) exit(EXIT_FAILURE);
+    ArrayList * nuevaLista = (ArrayList *) malloc(sizeof(ArrayList));   //Primero se reserva memoria para la lista
+    if (!nuevaLista) exit(EXIT_FAILURE);                                //y se comprueba que la reserva se haya hecho correctamente
 
-    nuevaLista->data = (void *) malloc(sizeof(void *) * 2);
+    nuevaLista->data = (void *) malloc(sizeof(void *) * 2);             //Reservamos memoria para el arreglo que almacenará los datos
     if (!nuevaLista->data) exit(EXIT_FAILURE);
 
-    nuevaLista->capacity = 2;
+    nuevaLista->capacity = 2;       //Inicializacion de los parametros de la lista
     nuevaLista->size = 0;
 
     return nuevaLista;
 }
 
 void append(ArrayList * l, void * data){
-    if (l->size == l->capacity){
-        l->capacity *= 2;
+    if (l->size == l->capacity){        //En esta condicional verificamos si el arreglo esta lleno
+        l->capacity *= 2;               //De ser así, se incrementa su tamaño al doble 
 
         l->data = (void *) realloc(l->data, sizeof(void*) * l->capacity);
         if (!l->data) exit(EXIT_FAILURE);
     }
 
-    l->data[l->size] = data;
+    l->data[l->size] = data;        //Guardamos el dato en la ultima posicion
 
-    l->size++;
+    l->size++;                      //Aumenta el size del arreglo
 }
 
 void push(ArrayList * l, void * data, int i){
-    if (i > l->size) return;
+    if (i > l->size) return;        //Comprobamos para un i válido
 
-    if (l->size == l->capacity){
-        l->capacity *= 2;
+    if (l->size == l->capacity){    //En esta condicional verificamos si el arreglo esta lleno
+        l->capacity *= 2;           //De ser así, se incrementa su tamaño al doble 
 
         l->data = (void *) realloc(l->data, sizeof(void*) * l->capacity);
         if (!l->data) exit(EXIT_FAILURE);
     }
 
     int k;
-    for (k = l->size - 1; k >= i; k--) l->data[k + 1] = l->data[k];
+    for (k = l->size - 1; k >= i; k--) {        //Bucle para mover los datos una casilla a la derecha
+        l->data[k + 1] = l->data[k];
+    }
 
     l->data[i] = data;
 
@@ -60,9 +62,9 @@ void* pop(ArrayList * l, int i){
 }
 
 void* get(ArrayList * l, int i){
-    if (i > l->size) return NULL;
-    
-    return (i < 0) ? l->data[l->size + i] : l->data[i];
+    if (i > l->size) return NULL;       //Verificamos que i sea válido
+
+    return (i < 0) ? l->data[l->size + i] : l->data[i];     //Retornamos el dato que corresponda segun el signo de i
 }
 
 int get_size(ArrayList * l){
